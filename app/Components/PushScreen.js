5a753fa762e9efd4e31593f5a62143535fc64ba7
77aram <77aram39@gmail.com>
2019-06-28 18:39:53 +0400

    Bugg

--- app/Components/PushScreen.js
+++ app/Components/PushScreen.js
@@ -3,26 +3,23 @@ import {Text, TextInput, View, Image, Button, StatusBar, StyleSheet, TouchableOp
 import ImagePicker from 'react-native-image-crop-picker';
 import Video from 'react-native-video';
 import axios from 'axios';
+import image from '../images/edit.png'
 
 class PushScreen extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            image: null
+            video: null,
+            image: image
         }
     }
 
-    useEffect() {
-        axios.post(`http://localhost:3000/uploadfile`)
-            .then(res => {
-                const image = res.data;
-                this.setState({image});
-            }, () => console.log('image', image))
-            .catch(err => {
-                console.log(err);
-            })
-    };
+    useEffect () {
+
+    }
+
+
 
     pickSingleWithCamera(cropping, mediaType = 'photo') {
         ImagePicker.openCamera({
@@ -31,24 +28,48 @@ class PushScreen extends React.Component {
             height: 500,
             includeExif: true,
             mediaType,
-        }).then(image => {
+        }).then(video => {
             // console.log('received image', image);
             this.setState({
-                image: {uri: image.path, width: image.width, height: image.height, mime: image.mime},
+                video: {uri: video.path, width: video.width, height: video.height, mime: video.mime},
                 images: null
             });
         }).catch(e => alert(e));
+
+
     }
 
-    renderAsset(image) {
-        if (image.mime && image.mime.toLowerCase().indexOf('video/') !== -1) {
-            return this.renderVideo(image);
+    renderAsset(video) {
+
+        if (video.mime && video.mime.toLowerCase().indexOf('video/') !== -1) {
+            return this.renderVideo(video);
+            let file = this.state.video;
+
+            let formdata = new FormData();
+
+            formdata.append('video', file)
+
+            axios({
+                url: 'http://localhost:3000/uploadfile',
+                method: 'POST',
+                headers: {
+                    authorization: 'Token'
+                },
+                data: formdata
+            }).then((res) => {
+                console.log(res)
+            }).catch(err => {
+                console.log(err)
+            })
+
         }
 
-        return this.renderImage(image);
+        return this.renderImage(video);
+
     }
 
     renderVideo(video) {
+
         console.log('rendering video');
         return (<View style={{height: 300, width: 300}}>
             <Video source={{uri: video.uri, type: video.mime}}
@@ -68,13 +89,14 @@ class PushScreen extends React.Component {
                    onLoad={load => console.log(load)}
                    repeat={true}/>
         </View>);
+
     }
 
     render() {
         return (
             <View>
                 <ScrollView>
-                    {this.state.image ? this.renderAsset(this.state.image) : null}
+                    {this.state.video ? this.renderAsset(this.state.video) : null}
                     {this.state.images ? this.state.images.map(i => <View
                         key={i.uri}>{this.renderAsset(i)}</View>) : null}
                 </ScrollView>
@@ -85,7 +107,6 @@ class PushScreen extends React.Component {
             </View>
         );
     }
-
 }
 
 const styles = StyleSheet.create({
