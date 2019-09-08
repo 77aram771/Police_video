d4f3565820fd3e88a8590482092b6d066bb0de1b
77aram771 <77aram39@gmail.com>
2019-09-09 00:37:07 +0400

    Fix

--- app/Components/PlayScreen.js
+++ app/Components/PlayScreen.js
@@ -10,8 +10,8 @@ import {
     Platform
 } from "react-native";
 
-
 const deviceWidth = Dimensions.get("window").width;
+
 
 const styles = StyleSheet.create({
     container: {
@@ -63,17 +63,27 @@ const styles = StyleSheet.create({
         borderColor: "#d1a667",
         borderWidth: 3,
         borderRadius: 2,
+        borderStyle: 'solid',
         height: 50,
         width: deviceWidth / 2,
         paddingVertical: 10,
         paddingHorizontal: 30,
         elevation: 4,
-        marginVertical: 10
+        marginVertical: 10,
+        alignContent: 'flex-start',
+
     },
     adminBtnContainer: {
         position: "absolute",
         top: 0,
         right: 0,
+        margin: 30,
+        marginTop: 60
+    },
+    CamBtnContainer: {
+        position: "absolute",
+        top: 0,
+        right: 50 + '%',
         margin: 30,
         marginTop: 60
     },
@@ -88,75 +98,84 @@ const styles = StyleSheet.create({
     btnText: {color: "#FFF", fontSize: 18}
 });
 
-const settings = {
-    camera: { cameraId: 1, cameraFrontMirror: true },
-    audio: { bitrate: 32000, profile: 1, samplerate: 44100 },
-    video: {
-        preset: 24,
-        bitrate: 400000,
-        profile: 2,
-        fps: 30,
-        videoFrontMirror: true
-    }
-};
-
 export default class PlayScreen extends Component {
     state = {
-        admin: true,
+        admin: false,
         isPublishing: false,
         userComment: "",
-        hasPermission: true,
-        paused: true
+        hasPermission: false,
+        paused: true,
+        cam: true,
+        camera: {cameraId: 0, cameraFrontMirror: true},
+        audio: {bitrate: 32000, profile: 1, samplerate: 44100},
+        video: {
+            preset: 24,
+            bitrate: 400000,
+            profile: 2,
+            fps: 30,
+            videoFrontMirror: true
+        }
     };
 
-    onPressAdminBtn = () => {
-        const {admin, hasPermission} = this.state;
-        this.setState({admin: !admin});
-        //console.log('admin', admin)
-        if (admin) {
-            //settings.camera.cameraId = 1;
-            console.log('settings.camera', settings.camera.cameraId);
+
+    changeCamera = async () => {
+        this.setState({
+            cam: !this.state.cam
+        });
+
+        if (this.state.cam) {
+            this.setState(prevState => ({
+                camera: {
+                    ...prevState.camera,
+                    cameraId: 0
+                }
+            }));
             if (Platform.OS === "android") {
-                if (!hasPermission) {
-                    this.checkPermissions();
+                if (this.state.hasPermission) {
+                    await this.checkPermissions();
                 }
             }
         }
         else {
-            //settings.camera.cameraId = 0;
-            console.log('settings.camera', settings.camera.cameraId)
+            this.setState(prevState => ({
+                camera: {
+                    ...prevState.camera,
+                    cameraId: 1
+                }
+            }));
+            if (Platform.OS === "android") {
+                if (!this.state.hasPermission) {
+                    await this.checkPermissions();
+                }
+            }
         }
-    };
 
-    onPressPlayBtn = () => {
-        const {paused: pausedState} = this.state;
-        this.setState({paused: !pausedState});
-    };
+    }
+
 
     renderCameraView = () => {
         const {hasPermission} = this.state;
         if (Platform.OS === "android" && !hasPermission) {
-            return <View />;
+            return (
+                <NodeCameraView
+                    style={styles.nodeCameraView}
+                    /* eslint-disable */
+                    ref={vb => {
+                        this.vb = vb;
+                    }}
+                    /* eslint-enable */
+                    outputUrl="rtmp://live.mux.com/app/STREAM-KEY-HERE"
+                    camera={{cameraId: 0, cameraFrontMirror: true}}
+                    audio={this.state.audio}
+                    video={this.state.video}
+                    autopreview
+                />
+            );
         }
-
-        return (
-            <NodeCameraView
-                style={styles.nodeCameraView}
-                /* eslint-disable */
-                ref={vb => {
-                    this.vb = vb;
-                }}
-                /* eslint-enable */
-                outputUrl="rtmp://live.mux.com/app/22d041c6-ed31-7801-d868-ea553a1fe671"
-                camera={settings.camera}
-                audio={settings.audio}
-                video={settings.video}
-                autopreview
-            />
-        );
     };
 
     checkPermissions = async () => {
+        console.log("Checking Permissions Android");
         try {
             const granted = await PermissionsAndroid.requestMultiple([
                 PermissionsAndroid.PERMISSIONS.CAMERA,
@@ -172,6 +191,7 @@ export default class PlayScreen extends Component {
                     hasAllPermissions = false;
                 }
             });
+            console.log("hasAllPermissions: ", hasAllPermissions);
             this.setState({hasPermission: hasAllPermissions});
         } catch (err) {
             console.warn(err);
@@ -182,7 +202,7 @@ export default class PlayScreen extends Component {
         const {isPublishing: publishingState, hasPermission} = this.state;
         if (Platform.OS === "android") {
             if (!hasPermission) {
-                this.checkPermissions();
+                await this.checkPermissions();
                 return;
             }
         }
@@ -197,33 +217,19 @@ export default class PlayScreen extends Component {
     };
 
     render() {
-        const {admin, isPublishing} = this.state;
+        const {isPublishing} = this.state;
         return (
             <View style={styles.container}>
-
                 {this.renderCameraView()}
-                <TouchableOpacity style={{
-                    justifyContent: 'flex-end',
-                    alignItems: 'flex-end',
-                    borderWidth: 1,
-                    borderStyle: 'solid'
-                }} onPress={this.onPressPublishBtn}>
+
+                <TouchableOpacity onPress={this.onPressPublishBtn}>
                     <View style={styles.goLive}>
                         <Text style={styles.btnText}>
                             {isPublishing ? "END LIVE" : "GO LIVE"}
                         </Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.adminBtnContainer}
-                    onPress={this.onPressAdminBtn}
-                >
-                    <View style={styles.adminBtn}>
-                        <Text style={styles.btnText}>
-                            {admin ? "Front Camera" : "Back Camera"}
-                        </Text>
-                    </View>
-                </TouchableOpacity>
+
             </View>
         );
     }
